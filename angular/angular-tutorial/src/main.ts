0c9559bc9d9b6f6d46bd248ffa632570b23e2b5b
Arun Kumar <java.kumar.arun@gmail.com>
2021-11-30 12:55:46 +0530

    angular

---
+++ angular/angular-tutorial/src/main.ts
@@ -0,0 +1,6 @@
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+// import { AppModule } from "./01-basic/app.module";
+// import { AppModule } from "./02-component/app.module";
+import { AppModule } from "./03-events/app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule)
